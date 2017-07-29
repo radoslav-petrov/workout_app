2460ae27274ca36cad82c4f906e07e43e1105865
Radoslav Petrov <radoslav.p.petrov@gmail.com>
2017-07-29 12:34:16 +0300

    Add user sign out feature spec

---
+++ spec/features/signing_users_out_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature 'Users Sign Out' do
+
+  before do
+    user = User.create(email: 'email@example.com', password: 'password')
+    login_as(user)
+  end
+
+  scenario 'via Sign out link' do
+    visit '/'
+
+    click_link 'Sign out'
+
+    expect(page).to have_content('Signed out successfully.')
+  end
+end
