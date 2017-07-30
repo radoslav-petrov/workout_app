1cc7d718cef163fdc09f6fd928b53105dac2d3a8
Radoslav Petrov <radoslav.p.petrov@gmail.com>
2017-07-30 20:43:19 +0300

    Update user model and add create action in exercieses controller

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -6,4 +6,26 @@ class ExercisesController < ApplicationController
   def new
     @exercise = current_user.exercises.new
   end
+
+  def create
+    @exercise = current_user.exercises.new(exercise_params)
+
+    if @exercise.save
+      flash[:success] = 'Exercise has been created'
+      redirect_to [current_user, @exercise]
+    else
+      flash.now[:danger] = 'Exercise has not been created'
+      render :new
+    end
+  end
+
+  private
+
+  def exercise_params
+    params.require(:exercise).permit(:duration_in_min, :workout, :workout_date, :user_id)
+  end
+
+  def find_exercise
+
+  end
 end
