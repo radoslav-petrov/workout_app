a6b2b56a1b0b6f7de56fbb986703647d416532e0
Radoslav Petrov <radoslav.p.petrov@gmail.com>
2017-07-30 20:14:19 +0300

    Add exercises routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :users do
+    resources :exercises
+  end
+
   root to: 'dashboard#index'
 end
